383b647b01f84f00d0fa3696b6edbccdcd8b3859
sverona <sasham@fastmail.com>
2019-09-30 13:00:31 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+#boorutf
+
+BooruTF is a Twitter fiction aggregator written in [Flask](https://flask.palletsprojects.com/en/).
+
+##Installation
+
+##Usage
+
+##Contribution
---
+++ test/test_server.py
@@ -0,0 +1,18 @@
+import unittest
+
+
+class TestServer(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        from boorutf import boorutf
+
+    def test_imports(self):
+        pass
+
+    def test_response(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
